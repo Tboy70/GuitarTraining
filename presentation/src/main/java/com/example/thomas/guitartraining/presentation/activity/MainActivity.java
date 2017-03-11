0324c6a31c73f6d724033b2a03bf652319404ec8
Thomas BOY <thomas.boy@orange.fr>
2017-03-11 20:13:12 +0100

    [FEAT] Mise en place du projet (packages + librairies)

---
+++ presentation/src/main/java/com/example/thomas/guitartraining/presentation/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.thomas.guitartraining.presentation.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.thomas.guitartraining.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
